ff739ebf2e5b9db345d8e2549248e07ab00cac64
kenkkui <thifirstime@gmail.com>
2024-05-31 14:43:00 +0800

    ready to transfer

--- public/app.js
+++ public/app.js
@@ -1,72 +1,60 @@
-// Index1
-// const [mouseOut, setMouseOut] = useState(false);
+// Elements
+const noButton = document.querySelector("button[data-no-button]");
+const mouseMsg = document.querySelector(".mouse-msg");
 
-// function handleMouseLeave() {
-//   setTimeout(() => {
-//     setMouseOut(true);
-//   }, 400);
-// }
+// Index1
+let mouseOut = false;
+
+function handleMouseLeave() {
+  setTimeout(() => {
+    mouseOut = true;
+  }, 400);
+}
 
 // Mouse leave msg
-// const [idle, setIdle] = useState(false);
-//   const element = useRef(null);
-//   const timeoutRef = useRef(null);
+let mouseIdle = false;
 
-//   useEffect(() => {
-//     const handlePointerMove = (e) => {
-//       const { clientX: x, clientY: y } = e;
+const handlePointerMove = (e) => {
+  const { clientX: x, clientY: y } = e;
 
-//       if (element.current) {
-//         element.current.animate(
-//           {
-//             left: `${x + 10}px`,
-//             top: `${y - 30}px`,
-//           },
-//           {
-//             duration: 480,
-//             fill: "forwards",
-//           }
-//         );
-//       }
+  if (element.current) {
+    element.current.animate(
+      {
+        left: `${x + 10}px`,
+        top: `${y - 30}px`,
+      },
+      {
+        duration: 480,
+        fill: "forwards",
+      }
+    );
+  }
 
-//       if (timeoutRef.current !== null) {
-//         clearTimeout(timeoutRef.current);
-//       }
+  if (timeoutRef.current !== null) {
+    clearTimeout(timeoutRef.current);
+  }
 
-//       timeoutRef.current = window.setTimeout(() => {
-//         setIdle(true);
-//       }, idleTime);
+  timeoutRef.current = window.setTimeout(() => {
+    setIdle(true);
+  }, idleTime);
 
-//       setIdle(false);
-//     };
+  setIdle(false);
+};
 
-//     document.body.addEventListener("pointermove", handlePointerMove);
-
-//     return () => {
-//       document.body.removeEventListener("pointermove", handlePointerMove);
-//       if (timeoutRef.current !== null) {
-//         clearTimeout(timeoutRef.current);
-//       }
-//     };
-//   }, []);
+document.body.addEventListener("pointermove", handlePointerMove);
 
 // Yes/No Buttons
-const noButtonRef = useRef(null);
-const yesButtonRef = useRef(null);
-
 function handleClickNo() {
-  if (noButtonRef.current) {
-    const computedStyle = window.getComputedStyle(noButtonRef.current);
-    const currOpacity = computedStyle.getPropertyValue("opacity");
-    if (currOpacity < "0.2") {
-      noButtonRef.current.style.display = "none";
-      if (yesButtonRef.current) {
-        yesButtonRef.current.style.scale = "1.6";
-      }
+  const computedStyle = window.getComputedStyle(noButtonRef.current);
+  const currOpacity = computedStyle.getPropertyValue("opacity");
+  if (currOpacity < "0.2") {
+    noButtonRef.current.style.display = "none";
+    if (yesButtonRef.current) {
+      yesButtonRef.current.style.scale = "1.6";
     }
-
-    noButtonRef.current.style.opacity = String(parseFloat(currOpacity) - 0.25);
-
-    noButtonRef.current.style.transform = "translateY(0)";
   }
+
+  noButtonRef.current.style.opacity = String(parseFloat(currOpacity) - 0.25);
+
+  noButtonRef.current.style.transform = "translateY(0)";
 }
