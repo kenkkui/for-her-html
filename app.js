a0cf3a33a6901b1542519c040dc2e610c8cd1b02
kenkkui <kenzlain@gmail.com>
2024-05-31 16:19:12 +0800

    done with html

---
+++ app.js
@@ -0,0 +1,6 @@
+const noBtn = document.querySelector(a[data - no - btn]);
+const yesBtn = document.querySelector(a[data - yes - btn]);
+
+noBtn.addEventListener("click", () => {
+  console.log("Im clicking");
+});
