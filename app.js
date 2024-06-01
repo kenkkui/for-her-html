4afb997d5f36c3ce2ba46f4a2145a21331234e6e
kenkkui <kenzlain@gmail.com>
2024-06-01 14:49:44 +0800

    ready to transfer

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const noBtn = document.querySelector(a[data - no - btn]);
-const yesBtn = document.querySelector(a[data - yes - btn]);
+const noBtn = document.querySelector("a[data-no-btn]");
+const yesBtn = document.querySelector("a[data-yes-btn]");
+const app = document.querySelector(".app");
 
 noBtn.addEventListener("click", () => {
-  console.log("Im clicking");
+  const opacity = window.getComputedStyle(noBtn).getPropertyValue("opacity");
+
+  if (parseFloat(opacity) < 0.3) {
+    const parent = noBtn.parentElement;
+    parent.remove();
+
+    yesBtn.parentElement.style.scale = "1.6";
+  }
+
+  noBtn.style.opacity = (parseFloat(opacity) - 0.4).toString();
 });
+
+const idleTime = 2400;
+let mouseIdle = false;
+
+function handlePointerMove(e) {
+  const { clientX: x, clientY: y } = e;
+
+  window.setTimeout(() => {
+    mouseIdle = true;
+  }, idleTime);
+
+  mouseIdle - false;
+
+  const newMouseMsg = document.createElement("div.mouse-msg");
+}
